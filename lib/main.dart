9dfd3a415eb184465a349aaa47645ec0660a023f
gcastro <gcastro@coppel.com>
2019-12-17 18:21:21 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+/* ------------------------------
+    Prueba de variantes con Hola Mundo
+    201912171158    GACC
+   ------------------------------ */
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        final wordPair = WordPair.random();
+
+        return MaterialApp(
+            title: 'Welcome to Flutter',
+            home: Scaffold(
+                appBar: AppBar(
+                    title: Text('Welcome to Flutter')
+                ),
+                body: Center(
+                    child: Text('Hello World !!'),
+                    //child: Text(wordPair.asPascalCase),
+                ),
+            ),
+        );
+    }
+}
+
